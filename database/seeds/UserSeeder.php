88f85b72b695d36f904f0175cd4a6b2d532425ca
nut2fellows <nuthugtshirt@gmail.com>
2019-06-13 16:23:28 +0700

    Update UserSeeder.php

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -18,7 +18,28 @@ class UserSeeder extends Seeder
         $user->password = \Hash::make('root');
         $user->tel = '00000000';
         $user->position = 'dev';
+        $user->permission = 'super admin';
+        $user->save();
+
+        $user = new User;
+        $user->username = 'nat2fellows';
+        $user->email = 'nat@2fellows.com';
+        $user->email_verified_at = \Carbon\Carbon::now();
+        $user->password = \Hash::make('admin');
+        $user->tel = '00000000';
+        $user->position = 'dev';
+        $user->permission = 'super admin';
+        $user->save();
+
+        $user = new User;
+        $user->username = 'max2fellows';
+        $user->email = 'max@2fellows.com';
+        $user->email_verified_at = \Carbon\Carbon::now();
+        $user->password = \Hash::make('admin');
+        $user->tel = '00000000';
+        $user->position = 'dev';
         $user->permission = 'admin';
         $user->save();
+
     }
 }
