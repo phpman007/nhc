cafaef56d85ff6230ea9808d9f706543a395952b
nut2fellows <nuthugtshirt@gmail.com>
2019-06-17 10:19:05 +0700

    migration
    อันนี้ขออนุญาต push ขึ้นเลยนะ

--- database/migrations/2019_06_12_112704_create_elections_table.php
+++ database/migrations/2019_06_12_112704_create_elections_table.php
@@ -15,7 +15,10 @@ class CreateElectionsTable extends Migration
     {
         Schema::create('elections', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('groupId')->comment("รหัสกลุ่ม");
+            $table->integer('groupId')->comment("รหัสกลุ่ม");
+            $table->integer('seniorGroupId')->comment("รหัสกลุ่มผู้ทรงคุณวุฒิ");
+            $table->integer('organizationGroupId')->comment("รหัสกลุ่มผู้แทนองค์กรบริหารส่วนท้องถิ่น");
+            $table->integer('ngoGroupId')->comment("รหัสกลุ่มผู้แทนองค์กรภาคเอกชน");
             $table->string('province')->comment("จังหวัด");
             $table->string('section')->comment("เขต");
             $table->date('openDate')->comment("วันที่เปิดรับสมัคร");
