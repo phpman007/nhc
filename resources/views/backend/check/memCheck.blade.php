f717689f9c5ec65ba441e517386fca89760b0b09
nut2fellows <nuthugtshirt@gmail.com>
2019-07-10 12:46:25 +0700

    Update

--- resources/views/backend/check/memCheck.blade.php
+++ resources/views/backend/check/memCheck.blade.php
@@ -1,5 +1,5 @@
 @extends('backend.theme.master')
-@section('title','NHC ADMIN')
+@section('title','NHCO ADMIN')
 
 @section('content')
 
@@ -157,7 +157,7 @@
                           @endif
                         </td>
                         <td align="middle">{{$valmember->docId}}</td>
-                        <td align="middle">{{$valmember->nameTitle}}{{$valmember->firstname}} {{$valmember->lastname}}</td>
+                        <td align="middle"><a href="/pdf/register/{{ $valmember->id }}">{{$valmember->nameTitle}}{{$valmember->firstname}} {{$valmember->lastname}}</a></td>
                         <td>{{$valmember->groupName}}</td>
                         <td align="middle">{{$valmember->province}}</td>
                         @if($valmember->zipFile==null)
